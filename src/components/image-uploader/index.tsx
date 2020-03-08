26c8beac640c4089569bae5ca9bf8cce551f06fa
Anton Ovechkin <owanturist@gmail.com>
2020-03-09 00:53:37 +0700

    I can't do it anymore

---
+++ src/components/image-uploader/index.tsx
@@ -0,0 +1,93 @@
+import Maybe, { Nothing } from 'frctl/Maybe';
+import * as React from 'react';
+import ReactDropzone, {
+  DropzoneInputProps as DropzoneInputProperties,
+} from 'react-dropzone';
+import styled, { StyledComponent } from 'styled-components';
+
+import { Dispatch, Effect } from '../core';
+
+interface StyledRootProps {
+  hovered: boolean;
+}
+
+const StyledRoot = styled.div<StyledRootProps>`
+  background: ${(properties: StyledRootProps): string =>
+    properties.hovered ? '#f8f8f8' : '#eee'};
+  border-radius: 3px;
+  color: #444;
+  cursor: default;
+  font-size: 18px;
+  outline: none;
+  padding: 20px;
+`;
+
+const StyledContent = styled.div`
+  align-items: center;
+  border: 2px dashed #aaa;
+  border-radius: 3px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  min-height: 300px;
+  padding: 20px;
+`;
+
+export interface Props {
+  children?: React.ReactNode;
+  onLoad(file: Maybe<File>): void;
+}
+
+interface State {
+  hovered: boolean;
+}
+
+export default class Dropzone extends React.PureComponent<Props, State> {
+  public readonly state: State = {
+    hovered: false,
+  };
+
+  private readonly onDragEnter: () => void;
+
+  private readonly onDragLeave: () => void;
+
+  private readonly onFileLoad: () => void;
+
+  public constructor(propserties: Props) {
+    super(propserties);
+
+    this.onDragEnter = (): void => this.setState({ hovered: true });
+
+    this.onDragLeave = (): void => this.setState({ hovered: false });
+
+    this.onFileLoad = (file: Maybe<File>): void => {
+      this.setState({ hovered: false });
+      this.props.onLoad(file);
+    };
+  }
+
+  public render(): React.ReactElement {
+    return (
+      <ReactDropzone
+        accept="text/plain"
+        multiple={false}
+        onDropAccepted={(files: File[]): void => {
+          this.onFileLoad(Maybe.fromNullable(files[0]));
+        }}
+        onDropRejected={(): void => this.onFileLoad(Nothing)}
+        onDragEnter={this.onDragEnter}
+        onDragLeave={this.onDragLeave}
+      >
+        {({ getRootProps, getInputProps }) => (
+          <StyledRoot {...getRootProps()} hovered={this.state.hovered}>
+            <input {...getInputProps()} />
+
+            <StyledContent>
+              <div>{this.props.children}</div>
+            </StyledContent>
+          </StyledRoot>
+        )}
+      </ReactDropzone>
+    );
+  }
+}
