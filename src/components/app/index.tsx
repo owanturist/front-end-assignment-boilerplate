0d5ccf6ace76427d08c014fe8107ff7cbecafcee
Anton Ovechkin <owanturist@gmail.com>
2020-03-09 16:23:48 +0700

    Make search by breed

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,5 +1,6 @@
 import { load as mobilenetLoad, MobileNet } from '@tensorflow-models/mobilenet';
 import Either, { Left, Right } from 'frctl/Either';
+import Decode from 'frctl/Json/Decode';
 import Maybe, { Just, Nothing } from 'frctl/Maybe';
 import RemoteData, { Failure, Loading, NotAsked, Succeed } from 'frctl/RemoteData/Optional';
 import * as React from 'react';
@@ -15,13 +16,18 @@ export interface Action {
   update(state: State): [State, Array<Effect<Action>>];
 }
 
-interface Classification {
-  className: string;
-  probability: number;
-}
-
 class Classify implements Action {
-  private constructor(private readonly result: Either<string, Classification[]>) { }
+  private static readonly decoder: Decode.Decoder<string[]> = Decode.field('status').string.chain(status => {
+    switch (status) {
+      case 'error': return Decode.field('message').string.chain(Decode.fail);
+
+      case 'success': return Decode.field('message').list(Decode.string);
+
+      default: return Decode.fail(`Unknown status "${status}"`);
+    }
+  })
+
+  private constructor(private readonly result: Either<string, string[]>) { }
 
   public static run(mobilenet: MobileNet, picture: string): Effect<Action> {
     return dispatch => {
@@ -32,8 +38,19 @@ class Classify implements Action {
 
       node.addEventListener('load', () => {
         mobilenet.classify(node, 1)
-          .then(classifications => dispatch(new Classify(Right(classifications))))
-          .catch(() => dispatch(new Classify(Left('Picture classification fails'))));
+          .then(classifications => {
+            if (classifications.length === 0) {
+              return Promise.reject('Classification is empty');
+            }
+
+            const [breed] = classifications[0].className.toLowerCase().split(/,\s*/u);
+
+            return fetch(`https://dog.ceo/api/breed/${breed}/images`)
+              .then(response => response.text())
+              .then(json => Classify.decoder.decodeJSON(json).mapLeft(error => error.stringify(4)))
+              .then(result => dispatch(new Classify(result)));
+          })
+          .catch(error => dispatch(new Classify(Left(String(error)))));
       })
     }
   }
@@ -43,7 +60,7 @@ class Classify implements Action {
       Left: error => [
         {
           ...state,
-          classifications: Failure(error)
+          sameBreedDogs: Failure(error)
         },
         [
           Toast.error(error).show()
@@ -53,7 +70,7 @@ class Classify implements Action {
       Right: classifications => [
         {
           ...state,
-          classifications: Succeed(classifications)
+          sameBreedDogs: Succeed(classifications)
         },
         []
       ]
@@ -117,7 +134,7 @@ class DropPicture implements Action {
     return [
       {
         ...state,
-        classifications: Loading
+        sameBreedDogs: Loading
       },
       [this.file.cata({
         Nothing: () => Toast.warning('It waits for pictures only').show(),
@@ -138,17 +155,24 @@ class LoadMobileNet implements Action {
   }
 
   public update(state: State): [State, Array<Effect<Action>>] {
-    return [
-      {
-        ...state,
-        mobilenet: RemoteData.fromEither(this.result)
-      },
-      Maybe.shape({
-        picture: state.picture,
-        mobilenet: this.result.toMaybe()
-      }).map(({ picture, mobilenet }) => [Classify.run(mobilenet, picture)])
-        .getOrElse([])
-    ]
+    return this.result.cata({
+      Left: error => [
+        state,
+        [
+          Toast.error(error).show()
+        ]
+      ],
+
+      Right: mobilenet => [
+        {
+          ...state,
+          mobilenet: Just(mobilenet)
+        },
+        state.picture.map(picture => [
+          Classify.run(mobilenet, picture)
+        ]).getOrElse([])
+      ]
+    });
   }
 }
 
@@ -156,15 +180,15 @@ class LoadMobileNet implements Action {
 
 export type State = Readonly<{
   picture: Maybe<string>;
-  mobilenet: RemoteData<string, MobileNet>;
-  classifications: RemoteData<string, Classification[]>;
+  mobilenet: Maybe<MobileNet>;
+  sameBreedDogs: RemoteData<string, string[]>;
 }>;
 
 export const init: [State, Array<Effect<Action>>] = [
   {
     picture: Nothing,
-    mobilenet: Loading,
-    classifications: NotAsked
+    mobilenet: Nothing,
+    sameBreedDogs: NotAsked
   },
   [
     LoadMobileNet.run
@@ -197,14 +221,12 @@ export const View = ({ state, dispatch }: Props) => (
       )
     })}
 
-    {state.classifications.cata({
-      Succeed: classifications => (
+    {state.sameBreedDogs.cata({
+      Succeed: sameBreedDogs => (
         <div>
-          {classifications.map(classification => (
-            <div key={classification.className}>
-              className: {classification.className}
-              <br />
-              probability: {classification.probability}
+          {sameBreedDogs.map(dog => (
+            <div key={dog}>
+              <img src={dog} />
             </div>
           ))}
         </div>
