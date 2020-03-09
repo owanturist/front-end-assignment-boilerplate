36832061ff7c6b88f82fba9a9fa369e657307ba3
Anton Ovechkin <owanturist@gmail.com>
2020-03-10 01:07:21 +0700

    Config prod build

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,12 @@
-const { join } = require('path');
-
 const autoPrefixer = require('autoprefixer');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
-const configuration = {
+module.exports = {
   cache: true,
-  devServer: {
-    compress: false,
-    contentBase: join(__dirname, 'dist'),
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    overlay: true,
-    writeToDisk: false,
-  },
-  devtool: 'cheap-module-eval-source-map',
   entry: {
     app: './src/index.tsx',
   },
-  mode: 'development',
   module: {
     rules: [
       {
@@ -45,20 +31,6 @@ const configuration = {
         ],
       },
       {
-        test: /\.css$/u,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [autoPrefixer()],
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
         test: /\.svg$/,
         use: [
           {
@@ -72,11 +44,6 @@ const configuration = {
       },
     ],
   },
-  output: {
-    filename: 'chunks/[name].js',
-    pathinfo: true,
-    publicPath: '/',
-  },
   performance: {
     hints: false,
   },
@@ -87,23 +54,9 @@ const configuration = {
       protectWebpackAssets: true,
       verbose: false,
     }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      minify: false,
-      template: 'src/index.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
   ],
   resolve: {
     extensions: ['.js', '.ts', '.tsx'],
     mainFields: ['browser', 'module', 'main'],
   },
-  watch: true,
-  watchOptions: {
-    ignored: /node_modules/u,
-  },
 };
-
-module.exports = configuration;
---
+++ webpack.dev.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    compress: false,
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    overlay: true,
+    writeToDisk: false,
+  },
+  output: {
+    filename: 'chunks/[name].js',
+    pathinfo: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/u,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoPrefixer()],
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      minify: false,
+      template: 'src/index.html',
+    }),
+  ],
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/u,
+  },
+});
